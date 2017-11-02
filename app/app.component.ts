54291ea1825e723e346cfc8c6889a42d2afa0cf4
Rob Maclean <rjmac@live.co.uk>
2017-11-02 16:17:42 +0000

    Added product comonent

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 
+
 @Component({
     selector: 'pm-app',
     template: `
-        <h1>Angular2: Getting Started</h1>
-    `
-})
-export class AppComponent { }
+    <div><h1>{{pageTitle}}</h1>
+        <pm-products></pm-products>
+    </div>
+    `})
+export class AppComponent {
+    pageTitle: string = "Acme Product Managment";
+}
